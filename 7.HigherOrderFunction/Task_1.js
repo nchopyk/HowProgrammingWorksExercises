603ab7dda1f732f7c6c6ed1d1781b60da196dda3
Nazar Chopyk <61637619+naz-olegovich@users.noreply.github.com>
2021-04-20 09:46:06 +0300

    Solved "HigherOrderFunction" exercises

---
+++ 7.HigherOrderFunction/Task_1.js
@@ -0,0 +1,9 @@
+const iterate = (obj, callback) => {
+    const keys = Object.keys(obj);
+    for (const key of keys) {
+        const value = obj[key];
+        callback(key, value, obj)
+    }
+};
+
+const obj = { a: 1, b: 2, c: 3 };
